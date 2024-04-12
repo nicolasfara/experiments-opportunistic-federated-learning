657ca66d582f04dbaf66432a6379a180054094cb
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-04-12 17:01:11 +0200

    style: reformat according to scalafmt

--- src/main/scala/it/unibo/scafi/OpportunisticFederatedLearning.scala
+++ src/main/scala/it/unibo/scafi/OpportunisticFederatedLearning.scala
@@ -1,6 +1,13 @@
 package it.unibo.scafi
+
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
-class OpportunisticFederatedLearning extends AggregateProgram with StandardSensors with ScafiAlchemistSupport with FieldUtils {
+
+class OpportunisticFederatedLearning
+    extends AggregateProgram
+    with StandardSensors
+    with ScafiAlchemistSupport
+    with FieldUtils {
+
   lazy val data = randomGen.nextDouble()
   trait Model {
     def eval(data: Double): Double
@@ -11,14 +18,16 @@ class OpportunisticFederatedLearning extends AggregateProgram with StandardSenso
     override def eval(data: Double): Double = data + id
     override def diff(model: Model): Double = id
   }
+
   override def main(): Any = {
-    val model = FooModel(mid())
-    val models =  excludingSelf.reifyField(nbr(model))
-    // first metric
-    val evaluations = models.map { case(id, model) => id -> model.eval(data) } // T
-    val neighEvals = excludingSelf.reifyField(nbr(evaluations)).map { case(id, evals) => id -> evals(id)} // T - 1
-    models.keys.map(id => id -> (evaluations(id) + neighEvals(id)) / 2).toMap
-    // second metric (discrepancy)
-    val discrepancies = models.map { case(id, model) => id -> model.diff(model) } // T
+    42
+//    val model = FooModel(mid())
+//    val models =  excludingSelf.reifyField(nbr(model))
+//    // first metric
+//    val evaluations = models.map { case(id, model) => id -> model.eval(data) } // T
+//    val neighEvals = excludingSelf.reifyField(nbr(evaluations)).map { case(id, evals) => id -> evals(id) } // T - 1
+//    models.keys.map(id => id -> (evaluations(id) + neighEvals(id)) / 2).toMap
+//    // second metric (discrepancy)
+//    val discrepancies = models.map { case(id, model) => id -> model.diff(model) } // T
   }
 }
