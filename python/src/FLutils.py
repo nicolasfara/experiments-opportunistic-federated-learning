d8457409c18a26502c37a810176872a746967fce
Davide Domini <davide.domini@unibo.it>
2024-04-17 11:26:52 +0200

    feat(python): implement evaluation

--- python/src/FLutils.py
+++ python/src/FLutils.py
@@ -147,3 +147,21 @@ def local_training(model, epochs, data, batch_size):
         mean_epoch_loss = sum(batch_loss) / len(batch_loss)
         epoch_loss.append(mean_epoch_loss)
     return model.state_dict(), sum(epoch_loss) / len(epoch_loss)
+
+def evaluate(model, data, batch_size):
+    criterion = nn.NLLLoss()
+    model.eval()
+    loss, total, correct = 0.0, 0.0, 0.0
+    data_loader = DataLoader(data, batch_size=batch_size, shuffle=False)
+    for batch_index, (images, labels) in enumerate(data_loader):
+        outputs = model(images)
+        batch_loss = criterion(outputs, labels)
+        loss += batch_loss.item()
+
+        _, pred_labels = torch.max(outputs, 1)
+        pred_labels = pred_labels.view(-1)
+        correct += torch.sum(torch.eq(pred_labels, labels)).item()
+        total += len(labels)
+
+    accuracy = correct / total
+    return accuracy, loss
