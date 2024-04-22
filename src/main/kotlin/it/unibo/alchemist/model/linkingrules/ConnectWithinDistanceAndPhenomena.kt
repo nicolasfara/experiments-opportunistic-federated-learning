5147fdd05d2d1f889a5cf02c7b324a6c8f5af36f
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-04-22 16:15:59 +0200

    feat: change connection linking rule

---
+++ src/main/kotlin/it/unibo/alchemist/model/linkingrules/ConnectWithinDistanceAndPhenomena.kt
@@ -0,0 +1,39 @@
+package it.unibo.alchemist.model.linkingrules
+
+import it.unibo.alchemist.model.Environment
+import it.unibo.alchemist.model.Neighborhood
+import it.unibo.alchemist.model.Node
+import it.unibo.alchemist.model.Position
+import it.unibo.alchemist.model.molecules.SimpleMolecule
+import it.unibo.alchemist.model.neighborhoods.Neighborhoods
+
+class ConnectWithinDistanceAndPhenomena<T, P : Position<P>>(
+    radius: Double,
+    private val nearestPhenomenaConnections: Int,
+) : ConnectWithinDistance<T, P>(radius) {
+    override fun computeNeighborhood(center: Node<T>, environment: Environment<T, P>): Neighborhood<T> {
+        val phenomenaNodes = environment.nodes.filter { it.contains(SimpleMolecule("Phenomena")) }
+        return when {
+            center.contains(SimpleMolecule("Phenomena")) -> {
+                val simpleNodes = environment.nodes.filter { !it.contains(SimpleMolecule("Phenomena")) }
+                val associations = simpleNodes.associateWith { n ->
+                    phenomenaNodes.map { p -> p to environment.getDistanceBetweenNodes(p, n) }
+                }.mapValues { (_, values) -> values.minBy { it.second } }
+                    .filter { (_, value) -> value.first == center }
+                    .keys
+                Neighborhoods.make(environment, center, associations)
+            }
+            else -> {
+                val nearestPhenomena = phenomenaNodes.asSequence()
+                    .map { it to environment.getDistanceBetweenNodes(center, it) }
+                    .sortedBy { it.second }
+                    .take(nearestPhenomenaConnections)
+                    .map { it.first }
+                super.computeNeighborhood(center, environment).apply {
+                    // removeAll { it.contains(SimpleMolecule("Phenomena")) }
+                    nearestPhenomena.forEach { this.add(it) }
+                }
+            }
+        }
+    }
+}
