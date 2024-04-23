d367e4b097dadec9d90d6c88950e40136af255c2
Davide Domini <davide.domini@unibo.it>
2024-04-23 10:09:38 +0200

    feat: smaller cnn

--- python/src/FLutils.py
+++ python/src/FLutils.py
@@ -15,8 +15,8 @@ class CNNMnist(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.fc1 = nn.Linear(320, 20)
+        self.fc2 = nn.Linear(20, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
