a83f79dd85eea7453e027854927261d5cf1be8ca
Davide Domini <davide.domini@unibo.it>
2024-04-29 12:43:16 +0200

    feat(python): implement ligther nn and switch to emnist dataset

--- python/src/FLutils.py
+++ python/src/FLutils.py
@@ -10,22 +10,18 @@ dataset_download_path = "../../build/dataset"
 
 class CNNMnist(nn.Module):
 
-    def __init__(self):
-        super(CNNMnist, self).__init__()
-        self.conv1 = nn.Conv2d(1, 5, kernel_size=5)
-        self.conv2 = nn.Conv2d(5, 10, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(160, 20)
-        self.fc2 = nn.Linear(20, 10)
+    def __init__(self, h1=128):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(28*28, h1)
+        self.fc2 = torch.nn.Linear(h1, 27)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, x.shape[1] * x.shape[2] * x.shape[3])
+        x = x.view(-1, 28 * 28)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        # x = F.sigmoid(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
 
 class DatasetSplit(Dataset):
     """An abstract Dataset class wrapped around Pytorch Dataset class.
@@ -91,7 +87,8 @@ def get_dataset(indexes):
 
     apply_transform = transforms.ToTensor()
 
-    train_dataset = datasets.MNIST(dataset_download_path,
+    train_dataset = datasets.EMNIST(dataset_download_path,
+                                   split='letters',
                                    train=True,
                                    download=False,
                                    transform=apply_transform)
@@ -105,7 +102,7 @@ def dataset_to_nodes_partitioning(areas: int, random_seed: int, shuffling: bool 
     np.random.seed(random_seed)  # set seed from Alchemist to make the partitioning deterministic
     apply_transform = transforms.ToTensor()
 
-    train_dataset = datasets.MNIST(dataset_download_path, train=True, download=True, transform=apply_transform)
+    train_dataset = datasets.EMNIST(dataset_download_path, split='letters', train=True, download=True, transform=apply_transform)
 
     # nodes_per_area = int(nodes_count / areas)
     dataset_labels_count = len(train_dataset.classes)
