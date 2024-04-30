f25027715d185d0266210a949cf41bffa67b3b59
Davide Domini <davide.domini@unibo.it>
2024-04-30 18:26:22 +0200

    fix: fix evaluate parameters

--- src/main/scala/it/unibo/alchemist/model/TestSetEvaluation.scala
+++ src/main/scala/it/unibo/alchemist/model/TestSetEvaluation.scala
@@ -3,7 +3,7 @@ package it.unibo.alchemist.model
 import it.unibo.alchemist.boundary.OutputMonitor
 import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.scafi.Sensors
-
+import me.shadaj.scalapy.py
 import scala.jdk.CollectionConverters.IteratorHasAsScala
 import it.unibo.scafi.interop.PythonModules.utils
 import me.shadaj.scalapy.py.PyQuote
@@ -14,12 +14,13 @@ class TestSetEvaluation[P <: Position[P]] extends OutputMonitor[Any, P] {
   private val seed = 1
 
   override def finished(environment: Environment[Any, P], time: Time, step: Long): Unit = {
+    println("Starting evaluation...")
     val layer = environment.getLayer(new SimpleMolecule(Sensors.testsetPhenomena)).get()
     val accuracies =
       nodes(environment)
       .map(node => {
-        val weights = node.getConcentration(new SimpleMolecule(Sensors.model))
-        val data = layer.getValue(environment.getPosition(node))
+        val weights = node.getConcentration(new SimpleMolecule(Sensors.model)).asInstanceOf[py.Dynamic]
+        val data = layer.getValue(environment.getPosition(node)).asInstanceOf[py.Dynamic]
         (weights, data)
       })
       .map { case (w, d) => evaluate(w, d) }
@@ -29,7 +30,7 @@ class TestSetEvaluation[P <: Position[P]] extends OutputMonitor[Any, P] {
   private def nodes(environment: Environment[Any, P]): List[Node[Any]] =
     environment.getNodes.iterator().asScala.toList
 
-  private def evaluate(weights: Any, data: Any): Double = {
+  private def evaluate(weights: py.Dynamic, data: py.Dynamic): Double = {
     val model = utils.nn_from_weights(weights)
     val result = utils.evaluate(model, data, batch_size, seed)
     val accuracy = py"$result[0]".as[Double]
