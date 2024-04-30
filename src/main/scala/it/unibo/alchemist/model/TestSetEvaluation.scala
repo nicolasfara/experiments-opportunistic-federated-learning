64bb03b319dce5be8f7d67d8b5017e460829c8f7
Davide Domini <davide.domini@unibo.it>
2024-04-30 16:27:29 +0200

    feat: implement testset output monitor

---
+++ src/main/scala/it/unibo/alchemist/model/TestSetEvaluation.scala
@@ -0,0 +1,39 @@
+package it.unibo.alchemist.model
+
+import it.unibo.alchemist.boundary.OutputMonitor
+import it.unibo.alchemist.model.molecules.SimpleMolecule
+import it.unibo.scafi.Sensors
+
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import it.unibo.scafi.interop.PythonModules.utils
+import me.shadaj.scalapy.py.PyQuote
+
+class TestSetEvaluation[P <: Position[P]] extends OutputMonitor[Any, P] {
+
+  private val batch_size = 64
+  private val seed = 1
+
+  override def finished(environment: Environment[Any, P], time: Time, step: Long): Unit = {
+    val layer = environment.getLayer(new SimpleMolecule(Sensors.testsetPhenomena)).get()
+    val accuracies =
+      nodes(environment)
+      .map(node => {
+        val weights = node.getConcentration(new SimpleMolecule(Sensors.model))
+        val data = layer.getValue(environment.getPosition(node))
+        (weights, data)
+      })
+      .map { case (w, d) => evaluate(w, d) }
+    accuracies.foreach(println(_))
+  }
+
+  private def nodes(environment: Environment[Any, P]): List[Node[Any]] =
+    environment.getNodes.iterator().asScala.toList
+
+  private def evaluate(weights: Any, data: Any): Double = {
+    val model = utils.nn_from_weights(weights)
+    val result = utils.evaluate(model, data, batch_size, seed)
+    val accuracy = py"$result[0]".as[Double]
+    accuracy
+  }
+
+}
