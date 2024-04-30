7543a14d75ad1a3bde7f213fa5b521afc1ce4026
davidedomini <davide.domini@unibo.it>
2024-04-30 22:20:01 +0200

    fix: fix get_dataset params

--- src/main/scala/it/unibo/alchemist/model/monitors/CentralizedTestSetEvaluation.scala
+++ src/main/scala/it/unibo/alchemist/model/monitors/CentralizedTestSetEvaluation.scala
@@ -11,12 +11,12 @@ import me.shadaj.scalapy.py
 class CentralizedTestSetEvaluation[P <: Position[P]] extends TestSetEvaluation[P] {
 
   override def finished(environment: Environment[Any, P], time: Time, step: Long): Unit = {
-    println("Starting evaluation...")
-    val weights =
-      nodes(environment).head.getConcentration(new SimpleMolecule(Sensors.model)).asInstanceOf[py.Dynamic]
-    val data = utils.get_dataset(Seq.empty[Int].toPythonProxy, false)
-    val accuracy = evaluate(weights, data)
-    TestDataExporter.CSVExport(List(accuracy), "data-baseline/test-accuracy")
+  println("Starting evaluation...")
+   val weights =
+     nodes(environment).head.getConcentration(new SimpleMolecule(Sensors.model)).asInstanceOf[py.Dynamic]
+   val data = utils.get_dataset(Seq.empty[Int].toPythonProxy, false, false)
+   val accuracy = evaluate(weights, data)
+   TestDataExporter.CSVExport(List(accuracy), "data-baseline/test-accuracy")
   }
 
 }
